77a8489b72548f435a5dd87ae167bfd75b4667c8
Subrata Malik <subratamalik1997@gmail.com>
2019-09-07 00:30:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/imageprocessing/MainActivity.java
@@ -0,0 +1,190 @@
+package com.example.imageprocessing;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.imageprocessing.R;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    //private static final String TAG = MainActivity.class.getSimpleName();
+
+    private int PICK_IMAGE_REQUEST = 1;
+    Bitmap bitmap;
+    ImageView click,imageView;
+    Button save;
+    BitmapDrawable drawable;
+    String ImagePath;
+    Uri URI;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView =(ImageView) findViewById(R.id.imgView);
+        click=(ImageView)findViewById(R.id.click) ;
+        save =(Button)findViewById(R.id.save);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                open();
+            }
+        });
+
+
+
+
+        ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        findViewById(R.id.bt1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+            }
+        });
+        findViewById(R.id.edit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Oops! Feature not available...", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+//                // TODO Auto-generated method stub
+                drawable =(BitmapDrawable)imageView.getDrawable();
+                Bitmap bitmapimg =drawable.getBitmap();
+                if (isExternalStorageWritable()) {
+                    saveImage(bitmapimg);
+                }else{
+                    Toast.makeText(MainActivity.this, "SDCard is not writable!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    //this method is used to save image to sdcard
+    private void saveImage(Bitmap finalBitmap) {
+
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root + "/saved_images");
+        myDir.mkdirs();
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String fname = "Alik_"+ timeStamp +".jpg";
+
+        File file = new File(myDir, fname);
+        if (file.exists()) file.delete ();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+            Toast.makeText(getApplicationContext(), "Image saved to "+root+"/saved_images", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+       //Camera
+        try {
+            Bitmap bitmap=(Bitmap)data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+           // Uri uri = data.getData();
+            //bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
+
+        // Gallery
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+
+
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+
+        }
+    }
+
+    public void open(){
+        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            requestCameraPermission();
+        }else
+        {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); //IMAGE CAPTURE CODE
+            startActivityForResult(intent, 1);
+        }
+    }
+
+    private void requestCameraPermission(){
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},1);
+    }
+
+}
+
