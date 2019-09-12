a3e0bb0dd8a7e604a00ca96934cf6c2610fcbb8e
Subrata Malik <subratamalik1997@gmail.com>
2019-09-12 20:47:31 +0530

    add image remove option

--- app/src/main/java/com/example/imageprocessing/MainActivity.java
+++ app/src/main/java/com/example/imageprocessing/MainActivity.java
@@ -58,7 +58,9 @@ public class MainActivity extends AppCompatActivity {
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showOptions();
+                if (save.isEnabled()) {
+                    showOptions();
+                }
             }
         });
 
